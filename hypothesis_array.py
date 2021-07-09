85431b1fec88bc3c85e052d03d14b406ef5eb2ef
Matthew Barber <quitesimplymatt@gmail.com>
2021-07-09 12:42:40 +0100

    stub float and bool inferred strategies

--- hypothesis_array.py
+++ hypothesis_array.py
@@ -1,4 +1,4 @@
-from typing import Optional, Union, TypeVar
+from typing import TypeVar
 
 from hypothesis import strategies as st
 
@@ -7,9 +7,14 @@ aa = None  # monkey patch this as the array module for now
 T = TypeVar("T")
 
 def from_dtype(dtype: T, **kwargs) -> st.SearchStrategy[T]:
-    if dtype in (aa.int8, aa.int16, aa.int32, aa.int64):
+    if dtype == aa.bool:
+        base_strategy = st.booleans(**kwargs)
+    elif dtype in (aa.int8, aa.int16, aa.int32, aa.int64):
         iinfo = aa.iinfo(dtype)
-        base_strategy = st.integers(min_value=iinfo.min, max_value=iinfo.min, **kwargs)
+        base_strategy = st.integers(min_value=iinfo.min, max_value=iinfo.max, **kwargs)
+    elif dtype in (aa.float32, aa.float64):
+        finfo = aa.finfo(dtype)
+        base_strategy = st.floats(min_value=finfo.min, max_value=finfo.max, **kwargs)
     else:
         raise NotImplementedError()
 
--- tests/test_dtypes.py
+++ tests/test_dtypes.py
@@ -5,17 +5,17 @@ from pytest import mark
 import hypothesis_array as aast
 
 # array api dtypes:
-# - int8"
-# - int16"
-# - int32"
-# - int64"
-# - uint8"
-# - uint16"
-# - uint32"
-# - uint64"
-# - float32"
-# - float64"
-# - bool"
+# - int8
+# - int16
+# - int32
+# - int64
+# - uint8
+# - uint16
+# - uint32
+# - uint64
+# - float32
+# - float64
+# - bool
 
 
 _module_dtypes = {
